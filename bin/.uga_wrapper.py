5fc3fbf16ab18d6c11486876d90082028922a772
ryan m koesterer <koesterr@scc-pi3.scc.bu.edu>
2015-03-05 14:47:24 -0500

    added new wrapper to bin

--- bin/.uga_wrapper.py
+++ bin/.uga_wrapper.py
@@ -8,29 +8,12 @@ import pwd
 import psutil
 import resource
 import subprocess
-import argparse
 import sys
-from uga.Model import Model
-from uga.Meta import Meta
-from uga.Map import Map
-from uga.Plot import Plot
-from uga.Process import kill_all
 from time import strftime, localtime, time, gmtime
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
-def main(args=None):
-	parser = argparse.ArgumentParser('quga')
-	parser.add_argument('--internal', 
-						action='store_true', 
-						help='job is internal to uga')
-	parser_required = parser.add_argument_group('required arguments')
-	parser_required.add_argument('--cmd', 
-						action='store',
-						required=True, 
-						help='python function command')
-
-	args=parser.parse_args()
+def main(argv):
 
 	start_time = (localtime(), time())
 	env_vars = os.environ.copy()
@@ -61,27 +44,23 @@ def main(args=None):
 	if 'SGE_TASK_ID' in env_vars.keys():
 		print "task index number: " + env_vars['SGE_TASK_ID']
 
-	##### FUNCTION TO RUN #####
-	if args.internal:
-		eval(args.cmd)
-	else:
-		try:
-			p = subprocess.Popen(args.cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
-			for line in iter(p.stdout.readline, ''):
-				sys.stdout.write(line)
-			p.wait()
-		except KeyboardInterrupt:
-			kill_all(p.pid)
-			print "\n   ... process terminated\n"
-			sys.exit(1)
+	if argv[1].split('(')[0] == "Model":
+		from uga.Model import Model
+	elif argv[1].split('(')[0] == "Meta":
+		from uga.Meta import Meta
+	elif argv[1].split('(')[0] == "Map":
+		from uga.Map import Map
+	elif argv[1].split('(')[0] == "Plot":
+		from uga.Plot import Plot
+
+	eval(argv[1])
 
 	end_time = (localtime(), time())
 	process = psutil.Process(os.getpid())
-	#mem = process.get_memory_info()[0] / float(2 ** 20)
 	mem=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0
 	print 'finish time: ' + strftime("%Y-%m-%d %H:%M:%S", end_time[0])
 	print 'time elapsed: ' + strftime('%H:%M:%S', gmtime(end_time[1] - start_time[1]))
 	print 'memory used: ' + str('%.2f' % mem) + ' MB'
 
 if __name__ == "__main__":
-	main()
+	main(sys.argv)
