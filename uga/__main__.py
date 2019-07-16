bc8811451cea09b3eedd3a89ba132f1216e5a4b4
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2019-07-16 10:57:17 -0400

    removed demandimports from main and from Dockerfile

--- uga/__main__.py
+++ uga/__main__.py
@@ -15,7 +15,6 @@
 ##    You should have received a copy of the GNU General Public License
 ##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import demandimport; demandimport.enable(); demandimport.ignore('bottleneck'); demandimport.ignore('pathlib')
 import os
 import numpy as np
 import pandas as pd
